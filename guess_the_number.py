f86d17b146b018780dd1c2c17d4914853dfa322a
Dilli <me@email.com>
2018-01-19 23:50:12 -0600

    Updated code

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,18 +5,18 @@ too_low = 'too low'
 too_high = 'too high'
 
 
-def configure_range():
+def configure_range(low, high):
     '''Set the high and low values for the random number'''
-    return 1, 10
+    return low, high
 
 
-def generate_secret(low, userRange):
+def generate_secret(low, high):
     '''Generate a secret number for the user to guess'''
-    return random.randint(low, userRange)
+    return random.randint(low, high)
 
 
 def get_guess():
-    '''get user's guess'''
+    '''get user's guess and handles exceptions.'''
     while True:
         try:
             userInput = int(input('Guess the secret number? '))
@@ -37,23 +37,31 @@ def check_guess(guess, secret):
 
 
 def main():
-    userRange = int(input("What range you want to generate? "))
-    guess_number = 0
-    (low, userRange) = configure_range()
-    secret = generate_secret(low, userRange)
+    print("Enter two different numbers to generate the range of secret number.")
+    #lower limit of the range.
+    lowerLimit = int(input("Enter the lower limit number: "))
+    #upper limit of the range.
+    upperLimit = int(input("Enter the upper limit number: "))
+    #initializes the counter.
+    guess_count = 0
+    configure_range(lowerLimit, upperLimit)
+    secret = generate_secret(lowerLimit, upperLimit)
 
     while True:
         guess = get_guess()
+        if (guess<lowerLimit or guess>upperLimit):
+            print("The number is out of range. Enter the number again.")
+            guess_count+=1
+        else:
+            result = check_guess(guess, secret)
+            print(result)
+            guess_count+=1
 
-        result = check_guess(guess, secret)
-        print(result)
-        guess_number+=1
-
-        if result == correct:
-            print('You had ', guess_number ,' tries!')
-            play_again = input("Play again! Please type 1 for no: ")
-            if play_again == 1:
-                break
+            if result == correct:
+                print('You had ', guess_count ,' tries!')
+        play_again = input("Play again! Please type y for yes: ")
+        if play_again != "y":
+            break
 
 
 
